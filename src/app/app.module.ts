fa24e38272e3257a35b4c58d59955224cac18fb6
Mohamed Rojbeni <rojbenimohamed@gmail.com>
2023-02-28 21:25:19 +0100

    add bootstrap support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
