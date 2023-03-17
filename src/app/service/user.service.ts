a69f6bf0d16e0f6019de6fe02c4bc48241e96a2f
Mohamed Rojbeni <rojbenimohamed@gmail.com>
2023-03-17 11:23:55 +0100

    tailwind integration

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -4,7 +4,10 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class UserService {
   constructor(private http: HttpClient) { }
+  
   creatUser(){
       console.log("hello world from mohamed");
   }
+
+
 }
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent {
+  countries$: Observable<Country[]>;
+  total$: Observable<number>;
+
+}
